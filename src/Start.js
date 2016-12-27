5a62fe7c6295ed916e3f6b54a031147221557548
Finn O'Connor <finneom@gmail.com>
2016-12-27 15:06:26 +1300

    Redundant code

--- src/Start.js
+++ src/Start.js
@@ -28,45 +28,5 @@ class button extends React.Component {
     )
   }
 }
-/*
-class App extends React.Component {
-  constructor(){
-    super();
-    this.state={
-      output: '', 
-      input: '//add jsx here',
-      error: ''
-    }
-  }
-  
-  update(e){
-    let code = e.target.value;
-    try {
-      this.setState({
-        output: window.Babel
-        .transform(code, {presets: ['es2015','react']}) //transforms our code using defined presets
-        .code,                                           //return that code to us.
-        error: ''
-      })
-    }catch(error){
-      this.setState({error: error.message})
-    }  
-  }
-  render(){
-    return (
-      <div>
-        <header>{this.state.error}</header>
-        <div className="container">
-          <textarea
-            onChange={this.update.bind(this)}
-            defaultValue={this.state.input}/>
-          <pre>
-            {this.state.output}
-          </pre>
-        </div>
-      </div>
-    )
-  }
-} 
-*/
+
 export default App
